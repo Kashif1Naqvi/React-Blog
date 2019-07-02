56beb0632c4d38e7b601d9224252c73f9d8bdf43
unknown <naqvishah582@gmail.com>
2019-07-03 00:13:27 +0500

    code update

--- src/components/Home/DataList.js
+++ src/components/Home/DataList.js
@@ -4,10 +4,10 @@ const DataList =(props)=> {
   return (
     <div className="col col-sm-3 col-md-3 col-xl-3 col-lg-3 pt-5 mt-5" >
       <img src={image}  alt="avatar" className="image-rounded   mt-5 pt-3 " width="100%" height="80%"  />
-      <p className="pt-3">{date}</p>
-      <h4 className=" pt-3" > {about}</h4>
-      <p>__</p>
-      <p className="pt-4 mt-4" >{category}</p>
+      <p className="text-center pt-3">{date}</p>
+      <h4 className="text-center  pt-3" > {about}</h4>
+      <p className="text-center  pt-3">__</p>
+      <p className="text-center pt-4 mt-4" >{category}</p>
     </div>
   )
 }
