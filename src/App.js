91b3b0354b7ce4aa133e3493d6300ff5f5d6fc6b
Kashif1Naqvi <naqvishah582@gmail.com>
2019-07-02 10:56:42 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as R,Route,Link } from 'react-router-dom'
+import './style.css'
+class App extends Component {
+  openNav = () => {
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  closeNav =()=> {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+  render(){
+    return (
+      <R>
+        <div className="container-fluid" >
+          <div id="mySidenav" className="sidenav">
+          <a href={e => e.preventDefault() } class="closebtn" onClick={this.closeNav}>&times;</a>
+            <div className="row">
+                <div className="col">
+                    <Link className="nav-item nav-link" >Home</Link>
+                    <Link className="nav-item nav-link" >Travel</Link>
+                    <Link className="nav-item nav-link" >Food & Drink</Link>
+                    <Link className="nav-item nav-link" >Style</Link>
+                    <Link className="nav-item nav-link" >Hostel</Link>
+                    <Link className="nav-item nav-link" >Diary</Link>
+                    <Link className="nav-item nav-link" >About</Link>
+                    <Link className="nav-item nav-link" >Contact</Link>
+                </div>
+                <div className="col">
+                  
+                    <Link>c</Link>
+                  
+                  <li>
+                    <Link>c</Link>
+                  </li>
+                  <li>
+                    <Link>c</Link>
+                  </li>
+                  <li>
+                    <Link>c</Link>
+                  </li>
+                  <li>
+                    <Link>c</Link>
+                  </li>
+                </div>
+            </div>
+          </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div id="main">
+        <span style={{fontSize:"30px",cursor:"pointer"}} onClick={this.openNav} >&#9776;</span>
+        </div>
+          
+        </div>
+      </R>
+    )
+  }
 }
-
-export default App;
+export default  App
